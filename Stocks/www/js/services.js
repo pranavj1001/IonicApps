b65ba8dcdfb3b205629f2ff26c3bde3ea5ba6665
pranavj1001 <pranavj1001@gmail.com>
2017-01-19 12:41:29 +0530

    add encodeURIService

--- Stocks/www/js/services.js
+++ Stocks/www/js/services.js
@@ -1,11 +1,22 @@
 angular.module('stocks.services', [])
 
-	.factory('stockDataService', function($q, $http){
+	.factory('encodeURIService', function(){
+
+		return{
+			encode: function(string) {
+			    return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+			}
+		};
+
+	})
+
+	.factory('stockDataService', function($q, $http, encodeURIService){
 
 		var getDetailsData = function(ticker){
 
 			var deffered = $q.defer(),
-			url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22" + ticker + "%22)&format=json&env=http://datatables.org/alltables.env";
+			query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+		    url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIService.encode(query) + '&format=json&env=http://datatables.org/alltables.env';
 			
 			$http.get(url)
     		.success(function(json){

880ea86fef9f47c3f9175b35ff347f2d30916d64
pranavj1001 <pranavj1001@gmail.com>
2017-04-10 03:05:53 +0530

    now the tabs can be moved right and left

--- MusicFinder/www/js/controllers.js
+++ MusicFinder/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('SearchCtrl',[ '$scope', '$ionicGesture' ,function($scope, $ionicGesture) {
+.controller('SearchCtrl', [ '$scope', '$ionicGesture', '$state' ,function($scope, $ionicGesture, $state) {
     
     $scope.hideHeader = function() {
         console.log("Hide");
@@ -16,10 +16,63 @@ angular.module('starter.controllers', [])
         });
     }
     
+    $scope.goLeft = function(){
+        console.log("Left");
+        //$(".view-container").hide('slide',{direction:'right'},200);
+        $state.go("tab.player");
+    }
+    
 }])
 
-.controller('PlayerCtrl', function($scope) {})
+.controller('PlayerCtrl', [ '$scope', '$ionicGesture', '$state', function($scope, $ionicGesture, $state) {
+    
+    $scope.hideHeader = function() {
+        console.log("Hide");
+        $( ".tabs" ).slideUp( 200, function() {
+           $( ".bar" ).slideUp(200); 
+        });
+    }
+    
+    $scope.showHeader = function() {
+        console.log("Show");
+        $( ".bar" ).slideDown( 200, function() {
+           $( ".tabs" ).slideDown(200); 
+        });
+    }
+    
+    $scope.goLeft = function(){
+        console.log("Left");
+        $state.go("tab.aboutme");
+    }
+    
+    $scope.goRight = function(){
+        console.log("Right");
+        $state.go("tab.search");
+    }
+    
+}])
 
-.controller('AboutMeCtrl', function($scope) {})
+.controller('AboutMeCtrl', [ '$scope', '$ionicGesture', '$state', function($scope, $ionicGesture, $state) {
+    
+    $scope.hideHeader = function() {
+        console.log("Hide");
+        $( ".tabs" ).slideUp( 200, function() {
+           $( ".bar" ).slideUp(200); 
+        });
+    }
+    
+    $scope.showHeader = function() {
+        console.log("Show");
+        $( ".bar" ).slideDown( 200, function() {
+           $( ".tabs" ).slideDown(200); 
+        });
+    }
+    
+    $scope.goRight = function(){
+        console.log("Right");
+        $state.go("tab.player");
+    }
+    
+}])
 
 ;

79431033ae6a548b3bf005884d57524497632805
pranavj1001 <pranavj1001@gmail.com>
2017-04-15 23:31:54 +0530

    add encodeURIService

--- MusicFinder/www/js/services.js
+++ MusicFinder/www/js/services.js
@@ -1,3 +1,17 @@
 angular.module('starter.services', [])
 
-.factory('AnyName', function() {});
+.factory('encodeURIService', function(){
+
+    return{
+        encode: function(string) {
+			 return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[!'()]/g, escape);
+        }
+    };
+
+})
+
+.factory('SongData', function($q, $http, encodeURIService) {
+    
+    
+    
+});
